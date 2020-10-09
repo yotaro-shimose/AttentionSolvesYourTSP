e6d1a876da6bbcabf36844a6fb23ca70178df1df
yotaro-shimose <ma-nu-@outlook.jp>
2020-10-09 18:12:26 +0900

    logger writes process id now

--- logger.py
+++ logger.py
@@ -1,12 +1,14 @@
 import tensorflow as tf
+import os
 
 
 class Logger:
 
     def __init__(self, logdir):
+        self.pid = os.getpid()
         self.writer = tf.summary.create_file_writer(logdir=logdir)
 
     def log(self, metrics, step):
         with self.writer.as_default():
             for key, value in metrics.items():
-                tf.summary.scalar(key, value, step)
+                tf.summary.scalar(str(self.pid) + key, value, step)
