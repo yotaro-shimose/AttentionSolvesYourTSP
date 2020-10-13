fixed mcts bug and cross_entropy calculation

agents/learner.py
@@ -2,9 +2,26 @@ import tensorflow as tf
 import time
 
 
-MASK_VALUE = -1
 # value to mask before softmax operation.
-INFINITE_VALUE = 1e+9  # see https://www.tensorflow.org/tutorials/text/transformer?hl=ja
+
+
+def masked_cross_entropy_from_Q(Q, Q_target, mask):
+    p_target = masked_softmax(Q_target, mask)
+    p = masked_softmax(Q, mask)
+    return -tf.math.reduce_mean(tf.keras.layers.dot([p_target, masked_log(p, mask)], axes=1))
+
+
+def masked_softmax(tensor, mask):
+    tensor = tensor
+    exps = tf.math.exp(tensor) * (1 - tf.cast(mask, tf.float32))
+    softmax = exps / tf.math.reduce_sum(exps, 1, keepdims=True)
+    return softmax
+
+
+def masked_log(tensor, mask):
+    float_mask = tf.cast(mask, tf.float32)
+    log = tf.math.log((1 - float_mask) * tensor + float_mask * 1)
+    return log
 
 
 # compute mask for trajectory with shape(batch_size, node_size)
@@ -36,7 +53,7 @@ class Learner:
         beta_q_first=1,
         beta_q_last=0.1,
         beta_a_first=0,
-        beta_a_last=0.045,
+        beta_a_last=0.45,
         annealing_step=int(1e4),
         weight_balancer=0.12,
     ):
@@ -152,18 +169,15 @@ class Learner:
 
         # Amortized Loss用のmask
         mask = create_mask(trajectory)
-        mask = tf.cast(mask, tf.float32) * -INFINITE_VALUE
-
-        mcts_policy = tf.nn.softmax(Q_mcts + mask)
 
         with tf.GradientTape() as tape:
+            # Calculate TDError
             Q_list = self.network([graph, trajectory])
             Q = tf.gather_nd(Q_list, indice)
             td_loss = self.huber(Q, target)
 
-            Q_list += mask
-            amortized_loss = tf.math.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-                mcts_policy, Q_list))
+            # Calculate masked cross entropy
+            amortized_loss = masked_cross_entropy_from_Q(Q_list, Q_mcts, mask)
             total_loss = self.beta_q * td_loss + self.beta_a * amortized_loss
             gradient = tape.gradient(total_loss, self.trainable_variables())
             self.optimizer.apply_gradients(

mcts/mcts.py
@@ -4,7 +4,7 @@ from copy import deepcopy
 
 AVERAGE_CONST = 0
 STANDARD_DEVIDATION = 0.000001
-C_PUCT = 0.001
+C_PUCT = 1
 
 
 class MCTS:
@@ -22,15 +22,14 @@ class MCTS:
     def search(self, env, search_num):
 
         trajectory = deepcopy(env.state.trajectory)
+
+        # 初めてきたSの場合、Q,NをNNで初期化
         if trajectory not in self.visited:
-            # Nの初期化
-            self.N[trajectory] = np.ones(len(trajectory))
-            self.Q[trajectory] = self.decoder([self.input, tf.constant(
-                [trajectory], dtype=tf.int32)]).numpy().squeeze()
-            self.visited.append(trajectory)
+            self.initialize_node(trajectory)
+
         # 決められた回数ゲームを行う
         for _ in range(search_num):
-            self.play_game(env)
+            self.play_game(deepcopy(env))
 
         return self.Q[trajectory]
 
@@ -38,16 +37,12 @@ class MCTS:
         '''
             現在の状態から再帰的にアクションを選択し、一回ゲームを終了させる
         '''
-
         trajectory = deepcopy(env.state.trajectory)
 
-        # 初めてきたSの場合、QをNNで初期化
+        # 初めてきたSの場合、Q,NをNNで初期化
         if trajectory not in self.visited:
-            # Nの初期化
-            self.N[trajectory] = np.ones(len(trajectory))
-            self.Q[trajectory] = self.decoder([self.input, tf.constant(
-                [trajectory], dtype=tf.int32)]).numpy().squeeze()
-            self.visited.append(trajectory)
+            # 初見のSのQ,NNを初期化しvisitedリストに追加
+            self.initialize_node(trajectory)
             return np.max(self.Q[trajectory] + trajectory.mask())
 
         # 次の選択を決める
@@ -73,3 +68,11 @@ class MCTS:
             self.N[trajectory][next_action]
 
         return total_reward
+
+    def initialize_node(self, trajectory):
+        # N, Qの初期化
+        self.N[trajectory] = np.ones(len(trajectory))
+        self.Q[trajectory] = self.decoder([self.input, tf.constant(
+            [trajectory], dtype=tf.int32)]).numpy().squeeze()
+        # visitedリストにSを追加
+        self.visited.append(trajectory)