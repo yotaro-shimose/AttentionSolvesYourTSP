2755a6883b36da4597944300518e48a33d342da4
yotaro-shimose <ma-nu-@outlook.jp>
2020-10-20 21:07:41 +0900

    added a comment on save_main.py (last one was not correct commit but no need to modify.

--- save_main.py
+++ save_main.py
@@ -1,5 +1,7 @@
 if __name__ == '__main__':
     import os
+    # works much faster on cpu probably because of the overhead copying values on gpu or tf.map_fn.
+    # hopefully it is due to tf.map_fn. Trying to replace create_mask function not to use tf.map_fn.
     os.environ["CUDA_VISIBLE_DEVICES"] = ""
     from gat.save.default_parameters import default_parameters
     from gat.save.save import SAVE
